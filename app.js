d6515df70534e825b36e2614cc2f258621c13dc4
Terri <terriyiu@gmail.com>
2022-11-12 22:53:57 -0500

    finished nav for mobile

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ portfolio.init = function(){
 
 portfolio.setUpEventListeners = function() {
     const navBtn = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.mobileNav');
 
     navBtn.addEventListener('click', function(){
         navBtn.classList.toggle('active');
+        navMenu.classList.toggle('navOpen');
     })
+    
 }
+   
 
 portfolio.init();
